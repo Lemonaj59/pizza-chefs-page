dfb5d61b35d8d49244cd7c67244683a5b9c11035
lemonaj58 <lemonaj58@gmail.com>
2022-11-09 01:11:23 -0700

    h

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname, "/build"));
+app.use(express.static(__dirname + "/build"));
 
 app.use(express.urlencoded({ extended: true }));
 
