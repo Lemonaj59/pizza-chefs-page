1b462149018d6e561d9f54c10a1678eb2498ac7a
lemonaj58 <lemonaj58@gmail.com>
2022-11-08 22:50:53 -0700

    h

--- index.js
+++ index.js
@@ -38,7 +38,13 @@ app.use(
 );
 
 app.use(function (req, res, next) {
-  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
   req.session;
   next();
 });
